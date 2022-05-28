e6d149917ebc2ace84163760ad5bea8dbd6578eb
Rémi Ait-Younes <34189296+Galacs@users.noreply.github.com>
2022-05-28 22:35:22 +0200

    Changement du nom des constantes
    Par conventions, dans le plupart des langages, on utilise des nom en majuscule pour représenter des constantes

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 # Indication des masses de base du proton et du neutron
-masse_protons = float(1.6726E-27)
-masse_neutron = float(1.6749E-27)
+MASSE_PROTONS = float(1.6726E-27)
+MASSE_NEUTRON = float(1.6749E-27)
 
 
 # L'utilisateur doit entrer un nombre de protons et de neutrons
@@ -9,10 +9,9 @@ n_neutrons = float(input("entrez le nombre de neutrons: "))
 
 
 # On cherche la masse totale que représente les protons et les neutrons
-m_p = masse_protons*n_protons
-m_n = masse_neutron*n_neutrons
+m_p = MASSE_PROTONS*n_protons
+m_n = MASSE_NEUTRON*n_neutrons
 
 
 # Calcul de la masse totale de l'atome
 print(m_p+m_n)
-
